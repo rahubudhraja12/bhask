4a808ff07610a16ef353e33ad5291222f09c575f
Baskar <baskarf@gmail.com>
2020-10-11 07:17:47 +0530

    test configured

--- src/test/java/com/rubix/inventorymanagement/InventorymanagementApplicationTests.java
+++ src/test/java/com/rubix/inventorymanagement/InventorymanagementApplicationTests.java
@@ -1,9 +1,11 @@
 package com.rubix.inventorymanagement;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+//@SpringBootTest
+@ContextConfiguration
 class InventorymanagementApplicationTests {
 
 	@Test
