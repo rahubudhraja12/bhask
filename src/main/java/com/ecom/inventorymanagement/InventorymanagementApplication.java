c775dab422b31e6780bb2f21dc0f5116db355264
Baskar <baskarf@gmail.com>
2020-10-11 08:32:41 +0530

    swagger changed

--- src/main/java/com/ecom/inventorymanagement/InventorymanagementApplication.java
+++ src/main/java/com/ecom/inventorymanagement/InventorymanagementApplication.java
@@ -19,7 +19,7 @@ public class InventorymanagementApplication {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.rubix.inventorymanagement")).build();
+				.apis(RequestHandlerSelectors.basePackage("com.ecom.inventorymanagement")).build();
 	}
 
 }
